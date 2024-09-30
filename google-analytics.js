a8c0704357b3e7bee61e149467aea7c8b278642d
tarlanlinazar <37047821+tarlanlinazar@users.noreply.github.com>
2024-09-30 14:49:33 +0400

    Create google-analytics.js

---
+++ google-analytics.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T7B8096X6Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T7B8096X6Y');
+</script>
